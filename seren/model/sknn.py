273b9d294dd5a418048f62a06eb25a56823b0cc2
amazingDD <1172392977@gmail.com>
2022-06-12 01:33:33 +0800

    change knn name & add TODO

--- seren/model/sknn.py
+++ seren/model/sknn.py
@@ -1,12 +1,39 @@
+'''
+@article{ludewig2018evaluation,
+  title={Evaluation of session-based recommendation algorithms},
+  author={Ludewig, Malte and Jannach, Dietmar},
+  journal={User Modeling and User-Adapted Interaction},
+  volume={28},
+  number={4},
+  pages={331--390},
+  year={2018},
+  publisher={Springer}
+}
+@inproceedings{sarwar2001item,
+  title={Item-based collaborative filtering recommendation algorithms},
+  author={Sarwar, Badrul and Karypis, George and Konstan, Joseph and Riedl, John},
+  booktitle={Proceedings of the 10th international conference on World Wide Web},
+  pages={285--295},
+  year={2001}
+}
+@inproceedings{aiolli2013efficient,
+  title={Efficient top-n recommendation for very large scale binary rated datasets},
+  author={Aiolli, Fabio},
+  booktitle={Proceedings of the 7th ACM conference on Recommender systems},
+  pages={273--280},
+  year={2013}
+}
+'''
+
 import numpy as np
-import pandas as pd
 import scipy.sparse as sp
-import scipy
 
 class SKNN(object):
     def __init__(self,config):
         '''
-        SessionKNN
+        SessionKNN Recommender, the similarity for session-based KNN can only be Jaccard,
+        Cosine similarity could be calculated if there are some duplicated items in one
+        session. Otherwise, Cosine is kind of similar to Jaccard
 
         Parameters
         -----------
@@ -24,19 +51,17 @@ class SKNN(object):
             The number of unique items in training set. 
         session_num : int
             The number of sessions in the training set.
-        
-
         '''
-        self.asymmetric_alpha = config['asymmetric_alpha']
-        self.similarity = config['similarity']
         self.k = config['k']
         self.shrink = config['shrink'] 
         self.normalize = config['normalize'] 
+        self.asymmetric_alpha = config['asymmetric_alpha']
+        self.similarity = config['similarity']
 
         self.item_num = config['item_num']
         self.session_num = config['session_num']
 
-        self.asymmetric_cosine = False
+        self.asymmetric_cosine = True if self.similarity == 'asymmetric' else False
 
     def fit(self, train_loader):
         '''
@@ -64,9 +89,11 @@ class SKNN(object):
         self.binary_train_matrix.data = np.ones_like(self.binary_train_matrix.data)
 
     def predict(self, input_ids, next_item):
+        # TODO
         pass
 
     def _compute_similarity(self):
+        # TODO 先把rank里昨晚说过的部分提取到这, 然后再写predict
         pass
 
 
@@ -84,6 +111,10 @@ class SKNN(object):
                 sim_vec = self._compute_jaccard(binary_new_session, self.binary_train_matrix)
             else:
                 sim_vec = self._compute_cosine(new_session, self.train_matrix)
+
+            if self.normalize:
+                sim_vec = sim_vec / np.sum(sim_vec)
+
             # K-NN for (session_num) array
             top_k_idx = (-sim_vec).argpartition(self.k-1)[0:self.k]
             mask = np.zeros_like(sim_vec)
@@ -100,26 +131,33 @@ class SKNN(object):
             res_ids.append(ids)
             res_scs.append(scs)
 
-    
     def _compute_cosine(self, session1, sessions):
+        ''' TODO 用latex 写一下sim的公式, 写完把这个todo删掉
+        cosine similarity = 
+        '''
         numerator = session1.dot(sessions.T).A.squeeze() # (1, item_num) (item_num, session_num) -> (1, session_num) -> (session_num,)
         s1_norm = np.sqrt(session1.power(2).sum()) # single value
         ss_norm = np.sqrt(sessions.power(2).sum(axis=1).A).reshape(-1)  # (session_num, 1) array -> (session_num,) array
         if self.asymmetric_cosine:
-            s1_norm = np.power(s1_norm + 1e-6, 2 * self.asymmetric_alpha)
-            ss_norm = np.power(ss_norm + 1e-6, 2 * (1 - self.asymmetric_alpha))
+            s1_norm = np.power(s1_norm + self.shrink + 1e-6, 2 * self.asymmetric_alpha)
+            ss_norm = np.power(ss_norm + self.shrink + 1e-6, 2 * (1 - self.asymmetric_alpha))
         denominator = s1_norm * ss_norm
         sim = numerator / denominator
 
         return sim
 
     def _compute_jaccard(self, session1, sessions):
-        # row, col = self.binary_train_matrix.nonzero()
+        ''' TODO 用latex 写一下sim的公式, 写完把这个todo删掉
+        jaccard similarity = 
+        '''
         nominator = session1.dot(self.binary_train_matrix.T).A.squeeze()
         batch_sum = (sp.vstack([session1 for _ in range(self.session_num)]) + sessions)
         batch_sum.data = np.ones_like(batch_sum.data)
         denominator = batch_sum.sum(axis = 1).A.reshape(-1)
-        return nominator / denominator
-  
-            
+        denominator = denominator + self.shrink + 1e-6
+        sim = nominator / denominator
+
+        return sim  
+
+# TODO sknn里的所有TODO都弄完后, 整理一下那个itemknn, 保证保留下的cosine, pearson, jaccard similarity代码逻辑和那个recsys2019的一致, 等我周日晚上弄完实习了再和你核对下
             
